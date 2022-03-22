89ffc1e3ebd82d8cb87ea35dc3d165ae48d84c09
加来大輔 <kakudaisuke@ksks-macbookpro.local>
2022-03-22 12:05:36 +0900

    modiry

--- main.py
+++ main.py
@@ -64,5 +64,5 @@ while True:
             r = coincheck.order(params)
             send_message_to_line(r)
             print('buy!!')
-            send_message_to_line("buy!!")
+            send_message_to_line('buy!!')
     df = df.iloc[1:, :]
